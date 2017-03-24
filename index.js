36d4de105fd3c4fd7159c2d3df0d0fe980f8e758
pbanfield <pbanfield@brightcove.com>
2017-03-24 16:40:40 -0700

    adding support for exclusions

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 var gitRevision = require('git-revision');
+var _ = require('lodash');
 var pjson;
+var exclusions;
 
 module.exports = WebpackSemverGit;
 
-function WebpackSemverGit(projectRoot) {
+function WebpackSemverGit(projectRoot, exlusions) {
+  exclusions = exclusions;
   pjson = require(projectRoot+'/package.json');
 }
 
 WebpackSemverGit.prototype.apply = function(compiler) {
   compiler.plugin('compilation', function(compilation) {
     compilation.plugin('chunk-hash', function(chunk, chunkHash) {
-      var shortHash = gitRevision("short");
-      chunkHash.digest = function() {
-        return pjson.version + "_" + shortHash;
-      };
+
+      console.log("chunk = " + chunk);
+      if(!_.includes(exclusions, chunk)){
+        var shortHash = gitRevision("short");
+        chunkHash.digest = function() {
+          return pjson.version + "_" + shortHash;
+        };
+      } else {
+        return chunk;
+      }
     });
   });
 };
