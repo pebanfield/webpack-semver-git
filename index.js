7f26f15ac9a40ebfed00ee1c6443e2876d923956
pbanfield <pbanfield@brightcove.com>
2017-03-25 11:36:09 -0700

    fixed pathing to work with context

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var gitRevision = require('git-revision');
 var _ = require('lodash');
 var pjson;
-var exclusions;
+var _projectRoot;
 
 module.exports = WebpackSemverGit;
 
-function WebpackSemverGit(projectRoot, exlusions) {
-  exclusions = exclusions;
-  pjson = require(projectRoot+'/package.json');
+function WebpackSemverGit(projectRoot) {
+  _projectRoot = projectRoot;
+  pjson = require(_projectRoot+'/package.json');
 }
 
 WebpackSemverGit.prototype.apply = function(compiler) {
@@ -16,11 +15,77 @@ WebpackSemverGit.prototype.apply = function(compiler) {
 
     compilation.plugin('chunk-hash', function(chunk, chunkHash) {
 
-      var shortHash = gitRevision("short");
+      var shortHash = getRev("short", _projectRoot);
       chunkHash.digest = function() {
         return pjson.version + "_" + shortHash;
       }
     });
   });
 };
+
+var process = require('child_process'),
+  exec = process.exec,
+  execSync = process.execSync;
+
+function parseOutput(output) {
+  return output.split('\n').join('')
+}
+
+/**
+ * The actual exported module
+ * @param  {string}     type the type of revision information wanted
+ * @param  {[Function]} cb either a callback (for async return) or nothing (for sync return)
+ * @return {[string]}   if sync, return a string
+ */
+function getRev(type, path, cb) {
+
+  // The actual shell command that will be run
+  var command
+
+  // Switch off the type and set the shell command
+  switch(type) {
+
+    // Short git SHA1 hash
+    case 'short':
+      command = 'git --git-dir ' + path + '/.git rev-parse --short HEAD'
+      break;
+
+    // Full git SHA1 hash
+    case 'hash':
+    case 'long':
+      command = 'git --git-dir ' + path + '/.git rev-parse HEAD'
+      break;
+
+    // The current tag if there is a tag,
+    //  otherwise, just returns the current hash
+    case 'tag':
+      command = 'git --git-dir ' + path + '/.git describe --always --tag --abbrev=0'
+      break;
+
+    // Current branch
+    case 'branch':
+      command = 'git --git-dir ' + path + '/.git rev-parse --abbrev-ref HEAD'
+      break;
+
+    // If the type doesn't match anything,
+    //  throw a ReferenceError
+    default:
+      throw ReferenceError('Revision type "' + type + '" is invalid')
+      break;
+  }
+
+  // If the callback is a function, run asynchronously
+  if(typeof cb == 'function')
+    exec(command, {cwd: __dirname}, function (err, stdout, stderr) {
+      if(err)
+        cb(parseOutput(stderr), err)
+      else
+        cb(parseOutput(stdout))
+    })
+
+  // Otherwise return the synchronous results
+  else
+    return parseOutput(execSync(command, {cwd: __dirname}).toString())
+
+}
 
